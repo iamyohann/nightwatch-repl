c55d5f39ca5e3c6b01f16ebe364903335fc2960b
Yohann <1330242+iamyohann@users.noreply.github.com>
2018-03-01 22:11:45 +1100

    Fixed issues with repl initialization (#3)

--- repl.js
+++ repl.js
@@ -4,7 +4,15 @@ exports.init = function _init(settings) {
     var commandPath = path.join(__dirname, '/commands');
 
     if (settings.custom_commands_path) {
-        settings.custom_commands_path.append(commandPath);
+
+        // If string, then convert to single element array
+        if(typeof settings.custom_commands_path === 'string') {
+            var existing = settings.custom_commands_path;
+            settings.custom_commands_path = [existing];
+        }
+
+        // append repl command path
+        settings.custom_commands_path.push(commandPath);
     } else {
         settings.custom_commands_path = [commandPath];
     }
